2614a16b9269602d2ce01504e2bb0262098a12bf
Naushad Ali <naushad.nizar@mobilelive.ca>
2024-01-18 23:14:37 +0500

    added react application

---
+++ apps/web-portal/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="Hire Guard" />
+    </div>
+  );
+}
+
+export default App;
